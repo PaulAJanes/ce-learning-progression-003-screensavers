4cbd3e90caa46132926f82f1261298339ac04a10
PaulAJanes <89553102+PaulAJanes@users.noreply.github.com>
2021-10-25 15:58:50 -0600

    Update microbit-program-8-2-2.js

--- programs/microbit-program-8-2-2.js
+++ programs/microbit-program-8-2-2.js
@@ -1,13 +1,13 @@
-namespace Complex{
+namespace Complex {
     export function add(Bobby: ComplexNumber, Alan: ComplexNumber, show: boolean = false): ComplexNumber {
         let Realie = Bobby.real + Alan.real;
         let Imajean = Bobby.imaginNum + Alan.imaginNum;
 
-        let imADjinn = Imajean.toString() + "i";
+        let imADjinn = "i" + Imajean.toString();
 
         let Zulu: ComplexNumber = new ComplexNumber(Realie, imADjinn);
 
-        if(show){
+        if (show) {
             showComplex(Bobby);
             Plus.showImage(0, 1500);
             showComplex(Alan);
@@ -18,19 +18,41 @@ namespace Complex{
         return Zulu;
     }
 
-    export function conjugate(Chuck: ComplexNumber): ComplexNumber{
-        return new ComplexNumber(Chuck.real, (Chuck.imaginNum * -1).toString() + "i");
+    export function conjugate(Chuck: ComplexNumber): ComplexNumber {
+        let holder = 0;
+        let retString = "";
+
+        holder = Chuck.imaginNum * -1;
+
+        if(holder < 0){
+            retString = "-i" + Chuck.imaginNum.toString();
+        } else {
+            retString = "i" + (-1*Chuck.imaginNum).toString();
+        }
+
+        return new ComplexNumber(Chuck.real, retString);
     }
 
-    export function multiply(David: ComplexNumber, Eddy: ComplexNumber): ComplexNumber{
+    export function multiply(David: ComplexNumber, Eddy: ComplexNumber): ComplexNumber {
+        let holder = 0;
+        let retString = "";
+
+        holder = (David.imaginNum * Eddy.real) + (Eddy.imaginNum * David.real);
+
+        if(holder < 0){
+            retString = "-i" + (-1*holder).toString();
+        } else {
+            retString = "i" + holder.toString();
+        }
+
         return new ComplexNumber((David.real * Eddy.real) +
             (David.imaginNum * Eddy.imaginNum * -1),
-            ((David.real * Eddy.imaginNum) + (David.imaginNum * Eddy.real)).toString() + "i");
+            retString);
     }
 
-    export function showComplex(Francis: ComplexNumber){
+    export function showComplex(Francis: ComplexNumber) {
         basic.showString(Francis.real.toString());
-        if(Francis.imaginary.substr(0, 1) != "-"){
+        if (Francis.imaginary.substr(0, 1) != "-") {
             Plus.showImage(0, 1500);
         }
         basic.showString(Francis.imaginary);
@@ -41,30 +63,34 @@ class ComplexNumber {
     private _real: number;
     private _imaginary: string;
 
-    constructor(real: number, imaginary: string){
+    constructor(real: number, imaginary: string) {
         this._real = real;
         this._imaginary = imaginary;
     }
 
-    get real(){
+    get real() {
         return this._real;
     }
 
-    set real(NewReal: number){
+    set real(NewReal: number) {
         this._real = NewReal;
     }
 
-    get imaginary(): string{
+    get imaginary(): string {
         return this._imaginary;
     }
 
-    set imaginary(NewImag: string){
+    set imaginary(NewImag: string) {
         this._imaginary = NewImag;
     }
 
-    get imaginNum(){
-        return parseInt(this._imaginary.substr(0, this._imaginary.length-1));
-    }   
+    get imaginNum(): number{
+        if(this.imaginary.charAt(0) == '-'){
+            return -1*parseInt(this.imaginary.substr(2, this.imaginary.length-1));
+        } else {
+            return parseInt(this.imaginary.substr(1, this.imaginary.length-1));
+        }
+    }
 }
 
 let Plus: Image = images.createImage(`00000
@@ -79,8 +105,8 @@ let Mult: Image = images.createImage(`00000
                                       01010
                                       00000`)
 
-let George: ComplexNumber = new ComplexNumber(6, "7i");
-let Howard: ComplexNumber = new ComplexNumber(-8, "-5i");
+let George: ComplexNumber = new ComplexNumber(6, "i7");
+let Howard: ComplexNumber = new ComplexNumber(-8, "-i5");
 
 Complex.add(George, Howard, true);
 
